562b4306dddf8adcb6cc4b69cff5097b6641315b
Alexei Pogodin <alex.pogodin.jr@gmail.com>
2023-12-10 21:52:42 +0100

    Special cards implementation
    Created special cards' implementation with some documentation.

---
+++ src/explodingkittens/_cards/defuse/DefuseCard.java
@@ -0,0 +1,17 @@
+package explodingkittens._cards.defuse;
+
+import explodingkittens._cards.std.BasicCard;
+
+/**
+ * A specific class that implements defuse
+ * card's functionality.
+ */
+public class DefuseCard extends BasicCard {
+    /**
+     * When player draws the exploding kitten card
+     * the defuse card is applied using this method.
+     */
+    public void use() {
+
+    }
+}
---
+++ src/explodingkittens/_cards/explode/ExplodingKittenCard.java
@@ -0,0 +1,17 @@
+package explodingkittens._cards.explode;
+
+import explodingkittens._cards.std.BasicCard;
+
+/**
+ * A specific class that implements exploding kitten
+ * card's functionality.
+ */
+public class ExplodingKittenCard extends BasicCard {
+    /**
+     * Kicks of the specified player if he doesn't have a
+     * defuse card.
+     */
+    public void explode() {
+
+    }
+}
