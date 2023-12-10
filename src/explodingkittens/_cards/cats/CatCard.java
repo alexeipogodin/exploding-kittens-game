3e500568fef268dbfcf2eb71d32435dae5ecf152
Alexei Pogodin <alex.pogodin.jr@gmail.com>
2023-12-10 21:50:44 +0100

    Cat cards implementation.
    Created cat cards' implementation with some documentation.

---
+++ src/explodingkittens/_cards/cats/CatCard.java
@@ -0,0 +1,21 @@
+package explodingkittens._cards.cats;
+
+import explodingkittens._cards.std.BasicCard;
+
+/**
+ * Implements all the simple cat cards
+ * and their specific functionality.
+ */
+public class CatCard extends BasicCard {
+    /**
+     * Contains names of action cards.
+     */
+    enum Cats {
+        BEARD_CAT,
+        CATTERMELON,
+        HAIRY_POTATO_CAT,
+        RAINBOW_RALPHING_CAT,
+        TACOCAT
+    }
+
+}
